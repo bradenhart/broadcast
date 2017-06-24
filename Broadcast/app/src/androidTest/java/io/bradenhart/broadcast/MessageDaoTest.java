164b57c239f8211e4b24d28d51c10a1d7372405d
BRADEN HART <bradendhart@gmail.com>
2017-06-24 23:39:45 +1200

    finished writing unit tests for message dao

--- Broadcast/app/src/androidTest/java/io/bradenhart/broadcast/MessageDaoTest.java
+++ Broadcast/app/src/androidTest/java/io/bradenhart/broadcast/MessageDaoTest.java
@@ -5,10 +5,7 @@ import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
-import junit.framework.Assert;
-
-import static org.hamcrest.Matchers.equalTo;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
 
 import org.junit.After;
 import org.junit.Before;
@@ -16,6 +13,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.io.IOException;
+import java.util.List;
 
 import io.bradenhart.broadcast.data.db.AppDatabase;
 import io.bradenhart.broadcast.data.db.dao.MessageDao;
@@ -42,34 +40,318 @@ public class MessageDaoTest {
         db.close();
     }
 
-    @Test
+//    @Test
     public void passes() {
-        Assert.assertEquals(1, 1);
+        assertEquals(1, 1);
     }
 
     @Test
-    public void insertsMessageIntoDatabaseAndRetrievesIt() {
-        // create a message with a title and content
-        String title = "Attn: Board Members";
-        String content = "There is a cyclone headed towards us.";
-        Message message = new Message(title, content);
-        // the message has no id (it's null)
-        // the id is created when the message is inserted into the db
+    public void createsDatabase() {
+        assertNotNull(db);
+    }
+
+    @Test
+    public void createsMessageDao() {
+        assertNotNull(messageDao);
+    }
+
+    /* inserting a message with a title and content succeeds */
+    @Test
+    public void insertsMessage_WithTitleAndContent_Succeeds() {
+        // create valid values for title and content
+        String title = "Valid title";
+        String content = "Valid content";
+
+        // create a message and set the title and content
+        Message message = new Message();
+        message.setTitle(title);
+        message.setContent(content);
+
+        // insert the message into the db, get the generated id
         long id = messageDao.insertMessage(message);
-        // set the id so that our assertion works below
-        message.setId(id);
 
-        // get the message from the db
-        Message byId = messageDao.getMessageById(id);
+        // retrieve the message we just inserted
+        Message retMessage = messageDao.getMessageById(id);
 
-        // the message we created should equal the message we retrieved as both will
-        // have the same id now
-        assertThat(byId, equalTo(message));
+        // make sure that the message fields are the values we expect
+        assertEquals(id, retMessage.getId());
+        assertEquals(title, retMessage.getTitle());
+        assertEquals(content, retMessage.getContent());
+    }
+
+    /* inserting a message with no title uses default value */
+    @Test
+    public void insertsMessage_WithNoTitle_UsesDefaultTitle() {
+        // create valid value for content
+        String content = "Valid content";
+
+        // create a message and set the content
+        Message message = new Message();
+        message.setContent(content);
+
+        // make sure that the message has the default title value
+        assertEquals(message.getTitle(), Message.BLANK_TITLE);
+
+        // insert the message into the db, get the generated id
+        long id = messageDao.insertMessage(message);
+
+        // retrieve the message we just inserted
+        Message retMessage = messageDao.getMessageById(id);
+
+        // make sure that the message fields are the values we expect
+        assertEquals(id, retMessage.getId());
+        assertEquals(Message.BLANK_TITLE, retMessage.getTitle());
+        assertEquals(content, retMessage.getContent());
+    }
+
+    /* inserting a message with no content uses default value */
+    @Test
+    public void insertMessage_WithNoContent_UsesDefaultContent() {
+        // create valid value for title
+        String title = "Valid title";
+
+        // create a message and set the title
+        Message message = new Message();
+        message.setTitle(title);
+
+        // make sure that the message has the default content value
+        assertEquals(message.getContent(), Message.BLANK_CONTENT);
+
+        // insert the message into the db, get the generated id
+        long id = messageDao.insertMessage(message);
+
+        // retrieve the message we just inserted
+        Message retMessage = messageDao.getMessageById(id);
+
+        // make sure that the message fields are the values we expect
+        assertEquals(id, retMessage.getId());
+        assertEquals(title, retMessage.getTitle());
+        assertEquals(Message.BLANK_CONTENT, retMessage.getContent());
+    }
+
+    /* inserting a message with no title and content uses default values */
+    @Test
+    public void insertsMessage_WithNoTitleOrContent_UsesDefaultValues() {
+        // create a message
+        Message message = new Message();
+
+        // make sure the message has the default values for title and content
+        assertEquals(message.getTitle(), Message.BLANK_TITLE);
+        assertEquals(message.getContent(), Message.BLANK_CONTENT);
+
+        // insert the message into the db, get the generated id
+        long id = messageDao.insertMessage(message);
+
+        // retrieve the message we just inserted
+        Message retMessage = messageDao.getMessageById(id);
+
+        // make sure that the message fields are the values we expect
+        assertEquals(id, retMessage.getId());
+        assertEquals(Message.BLANK_TITLE, retMessage.getTitle());
+        assertEquals(Message.BLANK_CONTENT, retMessage.getContent());
     }
 
     @Test
-    public void insertsSomeMessagesAndGetsAllFromDatabase() {
+    public void retrievesAllInsertedMessages() {
+        // create three messages
+        Message m1 = new Message();
+        m1.setTitle("title1");
+        m1.setContent("content1");
 
+        Message m2 = new Message();
+        m2.setTitle("title2");
+        m2.setContent("content2");
+
+        Message m3 = new Message();
+        m3.setTitle("title3");
+        m3.setContent("content3");
+
+        // insert three messages into db, get generated ids
+        long id1 = messageDao.insertMessage(m1);
+        assertEquals(1, id1);
+        long id2 = messageDao.insertMessage(m2);
+        assertEquals(2, id2);
+        long id3 = messageDao.insertMessage(m3);
+        assertEquals(3, id3);
+
+        // retrieve all of the messages
+        List<Message> retMessages = messageDao.getAllMessages();
+
+        // make sure that we have retrieved the correct amount of messages
+        assertEquals(3, retMessages.size());
+
+        // check m1
+        // make sure that the message fields are the values we expect
+        assertEquals(id1, retMessages.get(0).getId());
+        assertEquals("title1", retMessages.get(0).getTitle());
+        assertEquals("content1", retMessages.get(0).getContent());
+
+        // check m2
+        // make sure that the message fields are the values we expect
+        assertEquals(id2, retMessages.get(1).getId());
+        assertEquals("title2", retMessages.get(1).getTitle());
+        assertEquals("content2", retMessages.get(1).getContent());
+
+        // check m3
+        // make sure that the message fields are the values we expect
+        assertEquals(id3, retMessages.get(2).getId());
+        assertEquals("title3", retMessages.get(2).getTitle());
+        assertEquals("content3", retMessages.get(2).getContent());
+    }
+
+    @Test
+    public void updatesMessage_WithNewTitle() {
+        // create valid values for title and content
+        String title = "Valid title";
+        String content = "Valid content";
+
+        // create a message and set the title and content
+        Message message = new Message();
+        message.setTitle(title);
+        message.setContent(content);
+
+        // insert the message into the db, get the generated id
+        long id = messageDao.insertMessage(message);
+
+        // update the title
+        String updatedTitle = "Updated title";
+        message.setId(id); // set the id so the message can be updated
+        message.setTitle(updatedTitle);
+
+        // update the message
+        messageDao.updateMessage(message);
+
+        // retrieve the message
+        Message retMessage = messageDao.getMessageById(id);
+
+        // make sure that the message fields are the values we expect
+        assertEquals(id, retMessage.getId());
+        assertEquals(updatedTitle, retMessage.getTitle());
+        assertEquals(content, retMessage.getContent());
+    }
+
+    @Test
+    public void updatesMessage_WithNewContent() {
+        // create valid values for title and content
+        String title = "Valid title";
+        String content = "Valid content";
+
+        // create a message and set the title and content
+        Message message = new Message();
+        message.setTitle(title);
+        message.setContent(content);
+
+        // insert the message into the db, get the generated id
+        long id = messageDao.insertMessage(message);
+
+        // update the content
+        String updatedContent = "Updated content";
+        message.setId(id); // set the id so the message can be updated
+        message.setContent(updatedContent);
+
+        // update the message
+        messageDao.updateMessage(message);
+
+        // retrieve the message
+        Message retMessage = messageDao.getMessageById(id);
+
+        // make sure that the message fields are the values we expect
+        assertEquals(id, retMessage.getId());
+        assertEquals(title, retMessage.getTitle());
+        assertEquals(updatedContent, retMessage.getContent());
+    }
+
+    @Test
+    public void updatesMessage_WithNewTitleAndContent() {
+        // create valid values for title and content
+        String title = "Valid title";
+        String content = "Valid content";
+
+        // create a message and set the title and content
+        Message message = new Message();
+        message.setTitle(title);
+        message.setContent(content);
+
+        // insert the message into the db, get the generated id
+        long id = messageDao.insertMessage(message);
+
+        // update the title and content
+        String updatedTitle = "Updated title";
+        String updatedContent = "Updated content";
+        message.setId(id); // set the id so the message can be updated
+        message.setTitle(updatedTitle);
+        message.setContent(updatedContent);
+
+        // update the message
+        messageDao.updateMessage(message);
+
+        // retrieve the message
+        Message retMessage = messageDao.getMessageById(id);
+
+        // make sure that the message fields are the values we expect
+        assertEquals(id, retMessage.getId());
+        assertEquals(updatedTitle, retMessage.getTitle());
+        assertEquals(updatedContent, retMessage.getContent());
+    }
+
+    @Test
+    public void updatesMessage_WithNoChanges() {
+        // create valid values for title and content
+        String title = "Valid title";
+        String content = "Valid content";
+
+        // create a message and set the title and content
+        Message message = new Message();
+        message.setTitle(title);
+        message.setContent(content);
+
+        // insert the message into the db, get the generated id
+        long id = messageDao.insertMessage(message);
+
+        // don't update the title or the content
+        message.setId(id); // set the id so the message can be updated
+
+        // update the message
+        messageDao.updateMessage(message);
+
+        // retrieve the message
+        Message retMessage = messageDao.getMessageById(id);
+
+        // make sure that the message fields are the values we expect
+        assertEquals(id, retMessage.getId());
+        assertEquals(title, retMessage.getTitle());
+        assertEquals(content, retMessage.getContent());
+    }
+
+    @Test
+    public void deleteMessage() {
+        // create valid values for title and content
+        String title = "Valid title";
+        String content = "Valid content";
+
+        // create a message and set the title and content
+        Message message = new Message();
+        message.setTitle(title);
+        message.setContent(content);
+
+        // insert the message into the db, get the generated id
+        long id = messageDao.insertMessage(message);
+
+        // get all messages from the db
+        List<Message> retMessages = messageDao.getAllMessages();
+
+        // make sure that there is only one message
+        assertEquals(1, retMessages.size());
+
+        // delete the message
+        message.setId(id); // set the id so the message can be deleted
+        messageDao.deleteMessage(message);
+
+        // get all message from the db again
+        retMessages = messageDao.getAllMessages();
+
+        // make sure that there are no messages in the db
+        assertEquals(0, retMessages.size());
     }
 
 }
